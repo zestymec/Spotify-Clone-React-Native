967bae9d4f37e18b214a112aa47f4cb65d6cd778
zestymec <m.umer394aziz@gmail.com>
2026-04-21 16:45:35 +0500

    services added and seting up Track player

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './srs/App';
 import {name as appName} from './app.json';
+import TrackPlayer from 'react-native-track-player';
+import { PlaybackService } from './src/service';
 
 AppRegistry.registerComponent(appName, () => App);
--- srs/App.tsx
+++ srs/App.tsx
@@ -1,4 +1,5 @@
 import { StyleSheet, Text, View } from 'react-native'
+import TrackPlayer, { Capability } from 'react-native-track-player';
 import React from 'react'
 
 const App = () => {
